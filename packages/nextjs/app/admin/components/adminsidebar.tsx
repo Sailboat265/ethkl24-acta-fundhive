50d11477872437d8d3efe3410ce4d4312deb70cd
SamuelJsIntech <134241195+IonRipper@users.noreply.github.com>
2024-10-06 02:07:52 +0800

    Icon

--- packages/nextjs/app/admin/components/adminsidebar.tsx
+++ packages/nextjs/app/admin/components/adminsidebar.tsx
@@ -2,6 +2,9 @@
 import React from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { DocumentTextIcon, UserCircleIcon } from "@heroicons/react/24/outline";
+
+// Update the import paths
 
 const AdminSidebar = () => {
   const currentPath = usePathname(); // Get the current pathname
@@ -13,20 +16,22 @@ const AdminSidebar = () => {
         <li className="mb-2">
           <Link href="/admin/manage_user">
             <div
-              className={`w-full p-4 rounded-lg transition flex items-center justify-center 
+              className={`w-full p-4 rounded-lg transition flex items-center 
               ${currentPath === "/admin/manage_user" ? "bg-blue-600" : "bg-gray-700 hover:bg-blue-500"}`}
             >
-              <span className="text-lg">Manage Users</span> {/* Increased font size */}
+              <UserCircleIcon className="h-6 w-6 mr-2" /> {/* User Icon */}
+              <span className="text-lg">Users</span> {/* Increased font size */}
             </div>
           </Link>
         </li>
         <li className="mb-2">
           <Link href="/admin/manage_campaign">
             <div
-              className={`w-full p-4 rounded-lg transition flex items-center justify-center 
+              className={`w-full p-4 rounded-lg transition flex items-center 
               ${currentPath === "/admin/manage_campaign" ? "bg-blue-600" : "bg-gray-700 hover:bg-blue-500"}`}
             >
-              <span className="text-lg">Manage Campaigns</span> {/* Increased font size */}
+              <DocumentTextIcon className="h-6 w-6 mr-2" /> {/* Campaign Icon */}
+              <span className="text-lg">Projects</span> {/* Increased font size */}
             </div>
           </Link>
         </li>
